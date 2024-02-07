a603d56abbc075c28d04f754523f682945efae7c
Biju <biju.thomas@noaa.gov>
2024-02-07 16:59:39 +0000

    Reverting to the previous version of modulefile as the spack-stack-1.6.0 version did not succeed HAFS RT(memory-related Segmentation fault) on Jet

--- modulefiles/gsi_jet.lua
+++ modulefiles/gsi_jet.lua
@@ -1,25 +1,61 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/mnt/lfs4/HFIP/hfv3gfs/role.epic/hpc-stack/libs/intel-18.0.5.274/modulefiles/stack")
 
-local python_ver=os.getenv("python_ver") or "3.11.6"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.4.274"
+local cmake_ver=os.getenv("cmake_ver") or "3.20.1"
+local anaconda_ver=os.getenv("anaconda_ver") or "5.3.1"
+local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
 
-load(pathJoin("stack-intel", stack_intel_ver))
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-load(pathJoin("python", python_ver))
+load(pathJoin("hpc", hpc_ver))
+load(pathJoin("hpc-intel", hpc_intel_ver))
+load(pathJoin("hpc-impi", hpc_impi_ver))
 load(pathJoin("cmake", cmake_ver))
 
-load("gsi_common")
+prepend_path("MODULEPATH", "/contrib/anaconda/modulefiles")
+
+load(pathJoin("anaconda", anaconda_ver))
+
+local netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+
+local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
+local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.1"
+local sp_ver=os.getenv("sp_ver") or "2.3.3"
+local ip_ver=os.getenv("ip_ver") or "3.3.3"
+local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
+local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
+local nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+local wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
+local ncio_ver=os.getenv("ncio_ver") or "1.1.2"
+local crtm_ver=os.getenv("crtm_ver") or "2.4.0"
+local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.0"
+
+load(pathJoin("netcdf", netcdf_ver))
+
+load(pathJoin("bufr", bufr_ver))
+load(pathJoin("bacio", bacio_ver))
+load(pathJoin("w3emc", w3emc_ver))
+load(pathJoin("sp", sp_ver))
+load(pathJoin("ip", ip_ver))
+load(pathJoin("sigio", sigio_ver))
+load(pathJoin("sfcio", sfcio_ver))
+load(pathJoin("nemsio", nemsio_ver))
+load(pathJoin("wrf_io", wrf_io_ver))
+load(pathJoin("ncio", ncio_ver))
+load(pathJoin("crtm", crtm_ver))
+load(pathJoin("ncdiag",ncdiag_ver))
+
+
 load(pathJoin("prod_util", prod_util_ver))
 
 pushenv("CFLAGS", "-axSSE4.2,AVX,CORE-AVX2")
 pushenv("FFLAGS", "-axSSE4.2,AVX,CORE-AVX2")
 
-pushenv("GSI_BINARY_SOURCE_DIR", "/mnt/lfs4/HFIP/hfv3gfs/glopara/git/fv3gfs/fix/gsi/20230911")
+
+pushenv("GSI_BINARY_SOURCE_DIR", "/mnt/lfs4/HFIP/hfv3gfs/glopara/git/fv3gfs/fix/gsi/20230601")
 
 whatis("Description: GSI environment on Jet with Intel Compilers")
