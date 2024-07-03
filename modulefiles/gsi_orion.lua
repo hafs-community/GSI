4881781018640f5c2851dd33e994caef10d93ec9
Biju Thomas <biju.thomas@noaa.gov>
2024-07-03 14:15:57 -0500

    Re-enable HAFSv1 branch support for Orion after Rocky 9 OS upgrade

--- modulefiles/gsi_orion.lua
+++ modulefiles/gsi_orion.lua
@@ -1,26 +1,26 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.5.1/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/unified-env-rocky9/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("python_ver") or "3.10.8"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2022.0.2"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+local stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-local prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
+local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 load(pathJoin("python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
+load(pathJoin("prod_util", prod_util_ver))
 
 local netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
-local netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
-
-local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
+local netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
+local bufr_ver=os.getenv("bufr_ver") or "12.0.1"
 local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 local w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
-local sp_ver=os.getenv("sp_ver") or "2.3.3"
+local sp_ver=os.getenv("sp_ver") or "2.5.0"
 local ip_ver=os.getenv("ip_ver") or "4.3.0"
 local sigio_ver=os.getenv("sigio_ver") or "2.3.2"
 local sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
@@ -32,7 +32,6 @@ local ncdiag_ver=os.getenv("ncdiag_ver") or "1.1.2"
 
 load(pathJoin("netcdf-c", netcdf_c_ver))
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
-
 load(pathJoin("bufr", bufr_ver))
 load(pathJoin("bacio", bacio_ver))
 load(pathJoin("w3emc", w3emc_ver))
@@ -46,14 +45,14 @@ load(pathJoin("ncio", ncio_ver))
 load(pathJoin("crtm", crtm_ver))
 load(pathJoin("gsi-ncdiag",ncdiag_ver))
 
-load(pathJoin("prod_util", prod_util_ver))
+load("intel-oneapi-mkl/2022.2.1")
 
 pushenv("CFLAGS", "-xHOST")
 pushenv("FFLAGS", "-xHOST")
-
-pushenv("GSI_BINARY_SOURCE_DIR", "/work/noaa/global/glopara/fix/gsi/20230911")
+pushenv("GSI_BINARY_SOURCE_DIR", "/work/noaa/global/glopara/fix/gsi/20240208")
 
 whatis("Description: GSI environment on Orion with Intel Compilers")
 help([[
 Load common modules to build GSI on all machines
 ]])
+
