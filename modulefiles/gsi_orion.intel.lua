6f2aedb1c46a5e9f9423dcd1fbe4f606434be6c3
Biju Thomas <biju.thomas@noaa.gov>
2024-07-16 08:36:23 -0500

    Update MODULEPATH on RDHPCS platforms

--- modulefiles/gsi_orion.intel.lua
+++ modulefiles/gsi_orion.intel.lua
@@ -1,7 +1,7 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/gsi-addon-env-rocky9/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/unified-env-rocky9/install/modulefiles/Core")
 
 local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
