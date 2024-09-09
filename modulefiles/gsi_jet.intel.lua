7cbe413c08b65988c61841a9b7f4d8139e4bf81b
Biju <biju.thomas@noaa.gov>
2024-09-09 18:25:23 +0000

    Updating new impi version, impi/2024.2.1 on Jet

--- modulefiles/gsi_jet.intel.lua
+++ modulefiles/gsi_jet.intel.lua
@@ -6,11 +6,13 @@ prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.6.0/envs/unified-
 local python_ver=os.getenv("python_ver") or "3.11.6"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
+local impi_ver=os.getenv("impi_ver") or "2024.2.1"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("impi", impi_ver))
 load(pathJoin("python", python_ver))
 load(pathJoin("cmake", cmake_ver))
 
