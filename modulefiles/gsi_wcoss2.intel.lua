f8539f98e6e91479b235dc2599e89249d4c4af18
BijuThomas-NOAA <biju.thomas@noaa.gov>
2025-03-26 19:53:03 +0000

    Updating the python version(3.12.0) on WCOSS2

--- modulefiles/gsi_wcoss2.intel.lua
+++ modulefiles/gsi_wcoss2.intel.lua
@@ -6,7 +6,7 @@ local intel_ver=os.getenv("intel_ver") or "19.1.3.304"
 local craype_ver=os.getenv("craype_ver") or "2.7.17"
 local cray_mpich_ver=os.getenv("cray_mpich_ver") or "8.1.19"
 local cmake_ver= os.getenv("cmake_ver") or "3.20.2"
-local python_ver=os.getenv("python_ver") or "3.8.6"
+--local python_ver=os.getenv("python_ver") or "3.8.6"
 local prod_util_ver=os.getenv("prod_util_ver") or "2.0.14"
 local hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
 local netcdf_ver=os.getenv("netcdf_ver") or "4.9.2"
@@ -28,7 +28,7 @@ load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
 load(pathJoin("cray-mpich", cray_mpich_ver))
 load(pathJoin("cmake", cmake_ver))
-load(pathJoin("python", python_ver))
+--load(pathJoin("python", python_ver))
 
 load(pathJoin("prod_util", prod_util_ver))
 load(pathJoin("hdf5-A", hdf5_ver))
@@ -45,6 +45,9 @@ load(pathJoin("wrf_io", wrf_io_ver))
 load(pathJoin("ncio-A", ncio_ver))
 load(pathJoin("crtm", crtm_ver))
 load(pathJoin("ncdiag-A",ncdiag_ver))
+
+prepend_path("MODULEPATH", "/apps/dev/lmodules")
+load("intel/19.1.3.304/ve/hafs/2.1")
 
 pushenv("GSI_BINARY_SOURCE_DIR", "/lfs/h2/emc/global/noscrub/emc.global/FIX/fix/gsi/20240208")
 
