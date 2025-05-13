990ee1171376ca99046728dd2e38b5559b078cf6
Biju Thomas <biju.thomas@noaa.gov>
2025-05-13 13:44:03 -0400

    Deallocate the "array_lsw" array in read_gps.f90  before allocating it again if it is previously allocated.

--- src/gsi/read_gps.f90
+++ src/gsi/read_gps.f90
@@ -335,7 +335,11 @@ subroutine read_gps(nread,ndata,nodata,infile,lunout,obstype,twind, &
         if(ncnumgrp(ikx) > 0 )then                     ! cross validation on
            if(mod(nmrecs,ncnumgrp(ikx))== ncgroup(ikx)-1)usage=ncmiter(ikx)
         end if
-
+        
+! Deallocate the "array_lsw" array if it is previously allocated
+        if (allocated(array_lsw)) then
+           deallocate(array_lsw)
+        end if  
 ! New LSW Check 
         allocate(array_lsw(levs),array_fraclsw(levs),array_impact(levs), &
                  sorted_lsw(levs),sorted_fraclsw(levs),sorted_impact(levs), &
