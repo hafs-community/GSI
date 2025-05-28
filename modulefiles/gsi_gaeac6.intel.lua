bee7d3dc06f1002dfb97c874fbfb180e9dc9e622
Biju Thomas <biju.thomas@noaa.gov>
2025-05-28 10:54:48 -0400

    Update stack location to have bufr/12.0.1 version

--- modulefiles/gsi_gaeac6.intel.lua
+++ modulefiles/gsi_gaeac6.intel.lua
@@ -1,10 +1,9 @@
 help([[
 ]])
 
---prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/gsi-addon-dev/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/gsi-addon/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/autofs/ncrc-svm1_proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env-c6/install/modulefiles/Core")
 
-local stack_python_ver=os.getenv("stack_python_ver") or "3.11.6"
+local python_ver=os.getenv("python_ver") or "3.10.13"
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
 local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -12,7 +11,7 @@ local prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
-load(pathJoin("stack-python", stack_python_ver))
+load(pathJoin("python", python_ver))
 load(pathJoin("cmake", cmake_ver))
 
 load("gsi_common")
