d9ac8325b2b0928b104ae33a3aac72c1f528d05b
Biju Thomas <biju.thomas@noaa.gov>
2025-08-28 12:09:32 -0400

    Updating modulefiles(wgrib2 with support for ipolates and netcdf) on Gaea C6

--- modulefiles/gsi_gaeac6.intel.lua
+++ modulefiles/gsi_gaeac6.intel.lua
@@ -1,14 +1,14 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
 local stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
-local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.30"
+local stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.32"
 local cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 
-load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 load(pathJoin("stack-python", stack_python_ver))
 load(pathJoin("cmake", cmake_ver))
@@ -22,5 +22,5 @@ setenv("CMAKE_Fortran_COMPILER","ftn")
 setenv("CMAKE_CXX_COMPILER","CC")
 pushenv("CRAYPE_LINK_TYPE","dynamic")
 
-unload("cray-libsci")
+--unload("cray-libsci")
 whatis("Description: GSI environment on GaeaC6 with Intel Compilers")
